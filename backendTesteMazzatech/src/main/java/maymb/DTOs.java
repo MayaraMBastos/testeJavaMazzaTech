c0737f3e16d138455c2f04a5c5af2947183d430a
MayaraMB <maybastos2021@gmail.com>
2025-08-27 18:07:19 -0300

    feat: Estrutura inicial do projeto com Java 11 e dependências

---
+++ backendTesteMazzatech/src/main/java/maymb/DTOs.java
@@ -0,0 +1,4 @@
+package maymb;
+
+public class DTOs {
+}
---
+++ backendTesteMazzatech/src/main/java/maymb/Model/AgendamentoEntity.java
@@ -0,0 +1,92 @@
+package maymb.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class AgendamentoEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contaOrigem;
+
+    private String contaDestino;
+
+    private BigDecimal valorTransferencia;
+
+    private BigDecimal taxa;
+
+    private LocalDate dataTransferencia;
+
+    private LocalDate dataAgendamento;
+
+    public AgendamentoEntity(LocalDate dataAgendamento, LocalDate dataTransferencia, BigDecimal taxa, BigDecimal valorTransferencia, String contaDestino, String contaOrigem, Long id) {
+        this.dataAgendamento = dataAgendamento;
+        this.dataTransferencia = dataTransferencia;
+        this.taxa = taxa;
+        this.valorTransferencia = valorTransferencia;
+        this.contaDestino = contaDestino;
+        this.contaOrigem = contaOrigem;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContaOrigem() {
+        return contaOrigem;
+    }
+
+    public void setContaOrigem(String contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public String getContaDestino() {
+        return contaDestino;
+    }
+
+    public void setContaDestino(String contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+
+    public BigDecimal getValorTransferencia() {
+        return valorTransferencia;
+    }
+
+    public void setValorTransferencia(BigDecimal valorTransferencia) {
+        this.valorTransferencia = valorTransferencia;
+    }
+
+    public BigDecimal getTaxa() {
+        return taxa;
+    }
+
+    public void setTaxa(BigDecimal taxa) {
+        this.taxa = taxa;
+    }
+
+    public LocalDate getDataTransferencia() {
+        return dataTransferencia;
+    }
+
+    public void setDataTransferencia(LocalDate dataTransferencia) {
+        this.dataTransferencia = dataTransferencia;
+    }
+
+    public LocalDate getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(LocalDate dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
+}
